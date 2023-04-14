4c2137277d336919cf25e2cd387475a79780a057
Nikita Karasyov <nikitakarasev177@gmail.com>
2023-04-14 21:52:36 +0300

    fix

--- guides_platform/settings.py
+++ guides_platform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fy1t7x6#*ghy!)xn#ln9wzqka_lj6ug$l0wlnwbzfo7v=oq@ae'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
